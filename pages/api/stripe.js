d9764aad3782f24f0e95ce0a7f2e5e8c40479323
Aryankrgupta <aryankumargupta1106@gmail.com>
2023-03-13 21:12:37 +0530

    update

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
                         minimum: 1,
                     },
                     quantity: item.quantity
-                },
+                }
                 
             }),
 
